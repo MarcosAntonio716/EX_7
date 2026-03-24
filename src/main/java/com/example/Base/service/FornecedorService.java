04f8dc4c7adb678d73e91f04328f5dc0d72ff20d
unknown <marcosesteves2005@gmail.com>
2026-03-23 22:11:30 -0300

    Service

---
+++ src/main/java/com/example/Base/service/FornecedorService.java
@@ -0,0 +1,32 @@
+package com.example.Base.service;
+
+import com.example.Base.entity.Fornecedor;
+import com.example.Base.repository.FornecedorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FornecedorService {
+
+    @Autowired
+    private FornecedorRepository repository;
+
+    public Fornecedor salvar(Fornecedor fornecedor) {
+        return repository.save(fornecedor);
+    }
+
+    public List<Fornecedor> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<Fornecedor> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
